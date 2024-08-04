bfb81e18b2c7bdd1991cb550ad734051df747c30
Алексей Романов <saimonnice13@mail.ru>
2024-08-05 00:43:00 +0700

    Убрать лишние иморты

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -3,9 +3,6 @@ package ru.practicum.shareit.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.List;
-import java.util.Set;
-
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     boolean existsByEmail(String email);
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -15,7 +15,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
